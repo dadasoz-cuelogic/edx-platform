974df2feaa3e1013c9208b58aa017d96cc5226fc
Peter Fogg <pfogg@edx.org>
2016-06-09 10:21:24 -0400

    fixup! [safe templates] Commerce templates.

--- lms/static/js/commerce/views/receipt_view.js
+++ lms/static/js/commerce/views/receipt_view.js
@@ -39,7 +39,7 @@ var edx = edx || {};
                 courseKey: this.courseKey
             });
 
-            edx.HtmlUtils.setHtml(this.$el, _.template(templateHtml)(context));
+            edx.HtmlUtils.setHtml(this.$el, edx.HtmlUtils.template(templateHtml)(context));
 
             this.trackLinks();
 
